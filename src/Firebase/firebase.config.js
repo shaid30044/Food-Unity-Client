e7e2d025d8b102c2c600dd897b9a8d922efe9cef
shaid30044 <kaona30044@gmail.com>
2023-11-08 22:42:52 +0600

    create .env.local file for firebase key security

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2afh9D1qEEUuMZxNd09ArJBQbRvDf3lA",
-  authDomain: "assignment-11-6cede.firebaseapp.com",
-  projectId: "assignment-11-6cede",
-  storageBucket: "assignment-11-6cede.appspot.com",
-  messagingSenderId: "766171761718",
-  appId: "1:766171761718:web:73296feaf04ef6cc719c97",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
